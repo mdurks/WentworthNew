645baf89336bae153debd5fc326cbcc74f588a7b
Michael Durkin <mdurks@gmail.com>
2020-06-13 15:06:02 +0100

    remove old folder

--- src/srcs/pages/about.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Link } from 'gatsby';
-import Layout from '../components/layout.js';
-export default () => (
-  <Layout>
-    <h1>About page</h1>
-    <Link to="/">back to home</Link>
-  </Layout>
-);
