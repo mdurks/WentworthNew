8d087cc08df9713b8dab7a7dd59da8ab9f19b1df
Michael Durkin <mdurks@gmail.com>
2020-06-13 15:53:48 +0100

    test message for test branch

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,5 +5,6 @@ import Layout from '../components/layout.js';
 export default () => (
   <Layout>
     <h1>Welcome to the homepage of this test site</h1>
+    <p>This is a new branch called 'testbranch'</p>
   </Layout>
 );
